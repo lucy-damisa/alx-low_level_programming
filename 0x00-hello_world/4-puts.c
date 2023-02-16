6e0af0fa8fc3fcdfd74fcfbae4c9f2852fd75f54
lucy-damisa <damisalucy@gmail.com>
2023-02-16 15:17:32 -0800

    using puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
-}
-gcc program.c -o program
-./program
+gcc -o program program.c && ./program && echo 0
