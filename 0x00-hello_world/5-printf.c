562b69d75c57956a68c36c9f3e72d61259872877
lucy-damisa <damisalucy@gmail.com>
2023-02-16 16:06:47 -0800

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
