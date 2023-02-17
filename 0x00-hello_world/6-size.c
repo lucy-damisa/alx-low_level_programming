3cfca65bd19e1725e3b18b383ee24d9710413921
lucy-damisa <damisalucy@gmail.com>
2023-02-16 16:20:21 -0800

    print the size of the various part

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("The size of char is %lu bytes\n", sizeof(char));
+    printf("The size of short is %lu bytes\n", sizeof(short));
+    printf("The size of int is %lu bytes\n", sizeof(int));
+    printf("The size of long is %lu bytes\n", sizeof(long));
+    printf("The size of long long is %lu bytes\n", sizeof(long long));
+    printf("The size of float is %lu bytes\n", sizeof(float));
+    return 0;
+}
