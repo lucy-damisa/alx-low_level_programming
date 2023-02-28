5cc2578218031c9798ac52439d233838de0d1c5c
lucy-damisa <damisalucy@gmail.com>
2023-02-28 14:11:22 -0800

    array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
