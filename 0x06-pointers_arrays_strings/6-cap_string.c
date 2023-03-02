67ee6a812b2b10ca407b25ce04264ae661f7efb0
lucy-damisa <damisalucy@gmail.com>
2023-03-02 15:20:03 -0800

    arrays

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string
+ *
+ * Return: capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] = s[i] - ' '; /* a check 1st character */
+	i++;
+
+	while (s[i] != '\0') /* loop through the remaining characters */
+	{
+
+		/* capitalize all lowercase that comes after a special char*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - ' ';
+		i++;
+	}
+
+	return (s);
+}
